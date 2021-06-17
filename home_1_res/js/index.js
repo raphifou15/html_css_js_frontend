f7172c923e564b01087ddf055b3072894654a1d7
rkhelif <raphi031@hotmail.fr>
2021-06-17 18:25:05 +0200

    modif home

--- home_1_res/js/index.js
+++ home_1_res/js/index.js
@@ -11,8 +11,18 @@ const article2_1 = document.getElementsByClassName("article2-1");
 const article2_2 = document.getElementsByClassName("article2-2");
 let   largeur = window.innerWidth;
 let   inter;
-
-
+nou = article1_1[0].getBoundingClientRect();
+nou_2 = article2_1[0].getBoundingClientRect();
+if (nou.top < window.innerHeight){
+	section1.style.visibility = "visible";
+	if (window.innerWidth > 600)
+		article1_1[0].classList.add("move-in");
+}
+if (nou_2.top < window.innerHeight){
+	section2.style.visibility = "visible";
+	if (window.innerWidth > 600)
+		article2_1[0].classList.add("move-in");
+}
 
 
 largeur = (window.innerWidth > 600) ? largeur / 2 : largeur;
@@ -33,7 +43,22 @@ window.addEventListener('resize', () =>{
 	article2_2[0].style.height = largeur + "px";
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
+	nou = article1_1[0].getBoundingClientRect();
+	nou_2 = article2_1[0].getBoundingClientRect();
+	if (nou.top < window.innerHeight){
+		section1.style.visibility = "visible";
+		if (window.innerWidth > 600)
+			article1_1[0].classList.add("move-in");
+	}
+	if (nou_2.top < window.innerHeight){
+		section2.style.visibility = "visible";
+		if (window.innerWidth > 600)
+			article2_1[0].classList.add("move-in");
+	}
+});
+
+/*window.addEventListener('scroll', () => {
 	console.log(window.scrollY);
 	if (article1_1[0].classList.contains("move-in") === false)
 	{
@@ -54,3 +79,4 @@ window.addEventListener('scroll', () => {
 		}
 	}
 });
+*/
